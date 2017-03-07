5ae1ef6b41b030691d87aeadae8fd2d59a1edad3
zzzuka <zuka-monstr.yes@yandex.ru>
2017-03-07 22:25:16 +0300

    A, B added

---
+++ lab4/A.py
@@ -0,0 +1,27 @@
+def bfs(graph, start, visited = None):
+    if visited is None:
+        visited = set()
+    time = {start:0}
+    visited.add(start)
+    queue = [start]
+    while queue:
+        v = queue.pop(0)
+        for neighbour in graph[v]:
+            if neighbour not in visited:
+                visited.add(neighbour)
+                queue.append(neighbour)
+                time[neighbour] = time[v] + 1
+    return time
+def read_graph():
+    n, m = map(int, input().split())
+    A = [[] for i in range(n)]
+    for i in range(m):
+        v1, v2 = map(int, input().split())
+        A[v1].append(v2)
+        A[v2].append(v1)
+    return A
+
+A = read_graph()
+B = bfs(A, 0)
+for i in B:
+    print(B[i])
---
+++ lab4/B.py
@@ -0,0 +1,25 @@
+def bfs(graph, start, visited = None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    queue = [start]
+    while queue:
+        v = queue.pop(0)
+        for neighbour in graph[v]:
+            if neighbour not in visited:
+                visited.add(neighbour)
+                queue.append(neighbour)
+                print(v, neighbour)
+
+
+def read_graph():
+    n, m = map(int, input().split())
+    A = [[] for i in range(n)]
+    for i in range(m):
+        v1, v2 = map(int, input().split())
+        A[v1].append(v2)
+        A[v2].append(v1)
+    return A
+
+A = read_graph()
+bfs(A, 0)
