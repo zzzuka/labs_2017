95ba865a553444ad89bc24580f6605095ceff84a
zzzuka <zuka-monstr.yes@yandex.ru>
2017-03-12 00:49:55 +0300

    all done, except connected components & hamilton

--- lab5/6.py
+++ lab5/6.py
@@ -1,5 +1,3 @@
-__author__ = 'student'
-from heapq import *
 import matplotlib.pyplot as plt
 import networkx as nx
 
@@ -20,13 +18,15 @@ nx.draw_networkx_nodes(G, pos, node_color='r', node_size=100, alpha=0.8)
 nx.draw_networkx_edges(G, pos, width=1.0, alpha=0.5)
 nx.draw_networkx_labels(G,pos,font_size=12,font_family='Comic Sans MS')
 
-#вводить названия вершин, не номера
-v1, v2 = input().split()
-#way = nx.dijkstra_path(G, v.index(v1), v.index(v2))
-way = nx.dijkstra_path(G, v1, v2)
-nx.draw_networkx_nodes(G, pos, nodelist=way, node_color='g', node_size=100, alpha=0.8)
-for i in range(0, len(way)-1):
-    nx.draw_networkx_edges(G, pos, edgelist=[(way[i], way[i+1])], width=1, alpha=1, edge_color='g')
+nx.is_eulerian(G)
+if nx.is_eulerian(G):
+    print('Граф эйлеров')
+    d = nx.eulerian_circuit(G)
+    nx.draw_networkx_nodes(G, pos, nodelist=d, node_color='g', node_size=100, alpha=0.8)
+    for i in range(0, len(d) - 1):
+        nx.draw_networkx_edges(G, pos, edgelist=[(way[i], way[i + 1])], width=1, alpha=1, edge_color='g')
+else:
+    print('Граф не эйлеров')
 
 plt.axis('off')
 plt.savefig("simple_path.png") # сохранить как png файл
